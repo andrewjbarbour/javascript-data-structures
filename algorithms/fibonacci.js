3c4a417c21ad5a19a37052191fc4bed96b84f525
Andrew Barbour <barbouraj@gmail.com>
2021-04-30 06:13:26 -0700

    added dynamic fibonacci algorithm

---
+++ algorithms/fibonacci.js
@@ -0,0 +1,14 @@
+const memo = {};
+
+const fibonacci = num => {
+  let returnValue;
+  if (memo[num]){
+    returnValue = memo[num];
+  } else if (num === 0 || num === 1) {
+    returnValue = num;
+  } else {
+    returnValue = fibonacci(num - 1) + fibonacci(num -2);
+    memo[num] = returnValue;
+  }
+  return returnValue;
+}
