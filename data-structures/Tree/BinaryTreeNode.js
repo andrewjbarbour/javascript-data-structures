e9b62cf23e734f6638c07102c61accd4b0c7c4a1
Andrew Barbour <barbouraj@gmail.com>
2021-05-03 11:32:15 -0700

    added new Binary Tree Node methods

--- data-structures/Tree/BinaryTreeNode.js
+++ data-structures/Tree/BinaryTreeNode.js
@@ -7,6 +7,7 @@ export default class BinaryTreeNode{
         this.value = value;
         this.left = null;
         this.right = null;
+        this.parent = null;
     }
 
     setValue(value){
@@ -47,5 +48,31 @@ export default class BinaryTreeNode{
             traverse = traverse.concat(this.right.traverseInOrder());
         }
         return traverse;
+    }
+
+    nextNode(node){
+        if(node == null) return null;
+        if(node.right !== null){
+            return leftMostChild(node.right);
+        }
+        else{
+            let q = node;
+            let x = q.parent;
+            while (x !== null && x.left != q){
+                q = x;
+                x = x.parent;
+            }
+            return x;
+        }
+    }
+
+    leftMostChild(node){
+        if (node === null){
+            return null;
+        }
+        while (node.left !== null) {
+            node = node.left;
+        }
+        return node;
     }
 }
