d625760872f693362d79fb42eb6e905d9ebfe599
Andrew Barbour <barbouraj@gmail.com>
2021-05-06 13:00:05 -0700

    optimized algorithm

--- algorithms/math/sieveOfEratosthenes.js
+++ algorithms/math/sieveOfEratosthenes.js
@@ -1,4 +1,7 @@
 export const sieveOfEratosthenes = (limit) => {
+    /**
+     * Generates a list of all prime numbers up to a limit
+     */
     // Handle edge cases
     if (limit <= 1) {
       return [];
@@ -10,7 +13,7 @@ export const sieveOfEratosthenes = (limit) => {
     output[1] = false;
    
     // Iterate up to the square root of the limit
-    for (let i = 2; i < Math.pow(limit, 0.5); i++) {
+    for (let i = 2; i < Math.sqrt(limit); i++) {
       if (output[i] === true) {
         // Mark all multiples of i as non-prime
         for (let j = Math.pow(i, 2); j <= limit; j = j + i) {
@@ -19,7 +22,8 @@ export const sieveOfEratosthenes = (limit) => {
       }
     }
    
-    // Remove non-prime numbers
+    // Remove non-prime numbers 
+    
     return output.reduce((primes, current, index) => {
       if (current) {
         primes.push(index);
