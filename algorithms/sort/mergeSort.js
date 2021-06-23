87a1dfc6343397555fd3dd62bd52b69fefd0c0e0
Andrew Barbour <barbouraj@gmail.com>
2021-06-23 13:52:04 -0700

    added mergeSort algorithm

---
+++ algorithms/sort/mergeSort.js
@@ -0,0 +1,27 @@
+const mergeSort = (startArray) => {
+    const length = startArray.length
+    if(length===1){
+        return startArray;
+    }
+
+    const mid = Math.floor(length / 2);
+    const leftArray = startArray.slice(0, mid);
+    const rightArray = startArray.slice(mid, length);
+
+    return merge(mergeSort(leftArray), mergeSort(rightArray))
+}
+    
+    merge = (leftArray, rightArray) => {
+        const sortedArray = [];
+        while(leftArray.length > 0 && rightArray.length > 0){
+            if (leftArray[0] < rightArray[0]){
+                sortedArray.push(leftArray[0]);
+                leftArray.shift();
+            } else {
+                sortedArray.push(rightArray[0]);
+                rightArray.shift();
+            }
+        }
+        return sortedArray.concat(leftArray).concat(rightArray);
+    }
+
