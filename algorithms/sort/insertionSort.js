530ce1de632b4af1c7cf764ad2d9aadde7eb4c99
Andrew Barbour <barbouraj@gmail.com>
2021-06-24 08:07:29 -0700

    added insertionSort

---
+++ algorithms/sort/insertionSort.js
@@ -0,0 +1,15 @@
+const insertionSort = (array) => {
+    const length = array.length
+    for(let i = 1; i < length; i++){
+        let current = array[i]
+        let j = i-1;
+        while(j > -1 && current < array[j]){
+            array[j+1] = array[j];
+            j--;
+        }
+        array[j+1] = current;
+    }
+    return array;
+}
+
+console.log(insertionSort([321, 24, 5, 8, 11, 9443]))
