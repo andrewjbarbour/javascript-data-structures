5f6c7394b0a5fbc325d02daaa16720641fca64d6
Andrew Barbour <barbouraj@gmail.com>
2021-06-25 19:26:04 -0700

    updated binary search algorithm

--- algorithms/search/binarySearch.js
+++ algorithms/search/binarySearch.js
@@ -1,22 +1,17 @@
 const binarySearch = (array, target) => {
-    //A pared down binary search algorithm. Uncomment the logs below to log searching.
+    /** O(logN) for sorted array*/
     let startIndex = 0;
     let endIndex = array.length - 1;
-    let count = 0;
     while(startIndex <=endIndex){
         let middleIndex = Math.floor((startIndex + endIndex)/2);
-        count++;
         if(target === array[middleIndex]){
             return middleIndex;
-            //console.log(`Target ${target} found after ${count} iterations`);
         }
         else if(target > array[middleIndex]){
             startIndex = middleIndex + 1;
-            //console.log("Searching right side");
         }
-        else if(target < array[middleIndex]){
+        else{
             endIndex = middleIndex - 1;
-            //console.log("Searching left side");
         }
     }
     return null;
