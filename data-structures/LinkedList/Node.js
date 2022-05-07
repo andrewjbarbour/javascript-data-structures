c91ea452652f8be5865b0b18c278297388f5a156
Andrew Barbour <barbouraj@gmail.com>
2022-05-07 08:21:13 -0700

    add setPrevNode method to Node to support Doubly Linked List

--- data-structures/LinkedList/Node.js
+++ data-structures/LinkedList/Node.js
@@ -1,17 +1,26 @@
 export default class Node {
-    constructor(data){
-        this.data = data;
-        this.next = null;
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+    this.prev = null;
+  }
+  setNextNode(node) {
+    if (node instanceof Node || node === null) {
+      this.next = node;
+    } else {
+      throw new Error("Next node must be a member of the Node class");
     }
-    setNextNode(node){
-        if(node instanceof Node || node === null){
-            this.next = node;
-        } else{
-            throw new Error("Next node must be a member of the Node class");
-        }
-    }
+  }
 
-    getNextNode(){
-        return this.next;
+  setPrevNode(node) {
+    if (node instanceof Node || node === null) {
+      this.prev = node;
+    } else {
+      throw new Error("Previous node must be a member of the Node class");
     }
+  }
+
+  getNextNode() {
+    return this.next;
+  }
 }
