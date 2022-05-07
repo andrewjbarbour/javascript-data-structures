43333235daff0d39f212d8442dbad8ae5e9f2000
Andrew Barbour <barbouraj@gmail.com>
2022-05-07 13:14:22 -0700

    remove duplicate return statement in Binary Search

--- data-structures/Tree/BinarySearch.js
+++ data-structures/Tree/BinarySearch.js
@@ -1,24 +1,21 @@
 const binarySearch = (array, target) => {
-    //A pared down binary search algorithm. Uncomment the logs below to log searching.
-    let startIndex = 0;
-    let endIndex = array.length - 1;
-    let count = 0;
-    while(startIndex <=endIndex){
-        let middleIndex = Math.floor((startIndex + endIndex)/2);
-        count++;
-        if(target === array[middleIndex]){
-            return middleIndex;
-            //console.log(`Target ${target} found after ${count} iterations`);
-            return;
-        }
-        else if(target > array[middleIndex]){
-            startIndex = middleIndex + 1;
-            //console.log("Searching right side");
-        }
-        else if(target < array[middleIndex]){
-            endIndex = middleIndex - 1;
-            //console.log("Searching left side");
-        }
+  //A pared down binary search algorithm. Uncomment the logs below to log searching.
+  let startIndex = 0;
+  let endIndex = array.length - 1;
+  let count = 0;
+  while (startIndex <= endIndex) {
+    let middleIndex = Math.floor((startIndex + endIndex) / 2);
+    count++;
+    if (target === array[middleIndex]) {
+      //console.log(`Target ${target} found after ${count} iterations`);
+      return middleIndex;
+    } else if (target > array[middleIndex]) {
+      startIndex = middleIndex + 1;
+      //console.log("Searching right side");
+    } else if (target < array[middleIndex]) {
+      endIndex = middleIndex - 1;
+      //console.log("Searching left side");
     }
-    return null;
-}
+  }
+  return null;
+};
