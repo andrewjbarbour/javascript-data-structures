a475c8921d6ef9ef6157bdb0f07d4ee698d2fcf9
Andrew Barbour <barbouraj@gmail.com>
2022-05-08 15:30:37 -0700

    relocate routeExists.js to graph algorithms

---
+++ algorithms/graph/routeExists.js
@@ -0,0 +1,4 @@
+export const routeExists = (vertex1, vertex2) => {
+  //Given two vertices on a directed graph, determines if a route exists between them
+  return vertex1.edges.includes(vertex2) || vertex2.edges.includes(vertex2);
+};
