d55c3ef5bd3cb26817b116600e9d0d3e5213d2f7
Andrew Barbour <barbouraj@gmail.com>
2022-05-14 18:46:37 -0700

    link PriorityQueue.js

--- data-structures/Queue/Queue.js
+++ data-structures/Queue/Queue.js
@@ -1,45 +1,44 @@
-import LinkedList from './LinkedList.js';
+import LinkedList from "./LinkedList.js";
 
 export default class Queue {
-    constructor(maxSize = Infinity){
-        this.queue = new LinkedList();
-        this.maxSize = maxSize;
-        this.size = 0;
-    }
+  constructor(maxSize = Infinity) {
+    this.queue = new LinkedList();
+    this.maxSize = maxSize;
+    this.size = 0;
+  }
 
-    isEmpty(){
-        return this.size === 0;
-    }
+  isEmpty() {
+    return this.size === 0;
+  }
 
-    hasRoom(){
-        return this.size < this.maxSize;
-    }
+  hasRoom() {
+    return this.size < this.maxSize;
+  }
 
-    enqueue(data){
-        if(this.hasRoom()){
-            this.queue.addToTail(data);
-            this.size++;
-
-        } else {
-            throw new Error ("Queue is full");
-        }
+  enqueue(data) {
+    if (this.hasRoom()) {
+      this.queue.addToTail(data);
+      this.size++;
+    } else {
+      throw new Error("Queue is full");
     }
+  }
 
-    dequeue(){
-        if(!this.isEmpty()){
-            const data = this.queue.removeHead();
-            this.size--;
-            return data;
-        } else {
-            throw new Error ("Queue is empty");
-        }
+  dequeue() {
+    if (!this.isEmpty()) {
+      const data = this.queue.removeHead();
+      this.size--;
+      return data;
+    } else {
+      throw new Error("Queue is empty");
     }
+  }
 
-    peek(){
-        return !this.isEmpty() ? this.queue.head.data : null;
-    }
+  peek() {
+    return !this.isEmpty() ? this.queue.head.data : null;
+  }
 
-    print(){
-        return this.queue.printList();
-    }
+  print() {
+    return this.queue.printList();
+  }
 }
